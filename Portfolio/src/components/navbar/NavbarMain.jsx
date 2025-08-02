34bb5783b48c464efac713b3684b0dce77335c73
Achyut-shekhar <achyutshekhar54@gmail.com>
2025-08-02 12:52:29 +0530

    Fix import case sensitivity in NavbarMain.jsx
    Corrected the import statements for NavbarLogo and NavbarLinks to match the actual file names, resolving potential issues on case-sensitive file systems.

--- Portfolio/src/components/navbar/NavbarMain.jsx
+++ Portfolio/src/components/navbar/NavbarMain.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import NavbarLogo from "./navbarlogo";
-import NavbarLinks from "./navbarlink";
+import NavbarLogo from "./NavbarLogo";
+import NavbarLinks from "./Navbarlink";
 import NavbarBtn from "./Navbarbutton";
 import NavbarToggler from "./NavbarToggler";
 
