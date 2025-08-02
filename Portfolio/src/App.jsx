8916b322d75e251e9f10c7805b1e54ce75721117
Achyut-shekhar <achyutshekhar54@gmail.com>
2025-08-02 12:28:46 +0530

    Remove unused App.css import from App.jsx
    Deleted the import of App.css from App.jsx as it is not needed, possibly due to exclusive use of Tailwind for styling.

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css' // Optional: Remove if you’re only using Tailwind
 import NavbarMain from './components/navbar/navbarmain'
 import HeroMain from './components/heroSection/HeroMain'
 import SubHeroMain from './components/subHeroSection/SubHeroSection'
